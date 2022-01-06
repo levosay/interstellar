3825369e1aa0df65df5b8893ede99f8a1db91c01
levosay <ryuuzaki44l@gmail.com>
2022-01-06 22:27:14 +0300

    create: inputValue

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -2,11 +2,15 @@ import React, {useEffect, useRef, useState} from 'react'
 import { DataTracks } from './dataTracks'
 import AudioControls from '../AudioControls/AudioControls'
 import classes from './audioPlayer.module.sass'
+import audioMute from '../../img/audio-0.svg'
+import audioOn from '../../img/audio-100.svg'
+import audio66 from '../../img/audio-66.svg'
+import audio33 from '../../img/audio-33.svg'
 
 const AudioPlayer = () => {
   const [trackIndex, setTrackIndex] = useState(0)
   const [trackProgress, setTrackProgress] = useState(0)
-  const [volume, setVolume] = useState(25)
+  const [volume, setVolume] = useState(35)
   const [isPlaying, setIsPlaying] = useState(false)
 
   const { title, author, imgSrc, soundSrc } = DataTracks[trackIndex]
@@ -14,13 +18,11 @@ const AudioPlayer = () => {
   const audioRef = useRef(new Audio(soundSrc))
   const intervalRef = useRef()
   const isReady = useRef(false)
+  const nowVolume = useRef([])
 
   const { duration } = audioRef.current
 
-  const currentPercentage = duration
-    ? `${(trackProgress / duration) * 100}%`
-    : "0%"
-  // console.log(currentPercentage)
+
 
   const startTimer = () => {
     clearInterval(intervalRef.current)
@@ -76,6 +78,43 @@ const AudioPlayer = () => {
     return (
       <span className={classes.progressTime}>{`${addZero(minutes)}:${addZero(seconds)}`}</span>
     )
+  }
+
+  const showVolumeIcon = (volume = 0.25) => {
+    console.log(volume)
+    let elemVolumeIcon = audio33
+    if (volume > 0.8) {
+      elemVolumeIcon = audioOn
+    } else if (volume >= 0.34 && volume <= 0.8) {
+      elemVolumeIcon = audio66
+    } else if (volume > 0 && volume <= 0.33) {
+      elemVolumeIcon = audio33
+    } else if (volume === 0) {
+      elemVolumeIcon = audioMute
+    }
+    return (
+      <img
+        className={classes.volumeImg}
+        src={elemVolumeIcon}
+        alt={`volume: ${volume}`}
+        onClick={toMutedPlayer}
+      />
+    )
+  }
+  const toMutedPlayer = () => {
+    nowVolume.current.push(audioRef.current.volume)
+
+    if (nowVolume.current.length > 2) {
+      nowVolume.current.shift()
+    }
+
+    if (audioRef.current.volume === 0) {
+      setVolume(nowVolume.current[0] * 100)
+        audioRef.current.volume = nowVolume.current[0]
+    } else if (audioRef.current.volume > 0) {
+      setVolume(0)
+        audioRef.current.volume = 0
+    }
   }
 
   useEffect(() => {
@@ -134,7 +173,7 @@ const AudioPlayer = () => {
       <div className={classes.progressWrapper}>
         {showTime(audioRef.current.currentTime)}
         <input
-          className={classes.progress}
+          className={classes.progressInput}
           type="range"
           value={trackProgress}
           step="1"
@@ -144,17 +183,21 @@ const AudioPlayer = () => {
           onMouseUp={onScrubEnd}
           onKeyUp={onScrubEnd}
         />
-        {showTime(duration)}
+        {showTime(duration ? duration : 0)}
       </div>
-      <input
-        className={classes.volume}
-        type="range"
-        value={volume}
-        step="1"
-        min="0"
-        max="100"
-        onChange={(event => changeVolume(event.target.value / 100))}
-      />
+      <div className={classes.volumeWrapper}>
+        {showVolumeIcon(audioRef.current.volume)}
+        <input
+          className={classes.volumeInput}
+          type="range"
+          value={volume}
+          step="1"
+          min="0"
+          max="100"
+          onChange={(event => changeVolume(event.target.value / 100))}
+        />
+      </div>
+
 
     </div>
   )
